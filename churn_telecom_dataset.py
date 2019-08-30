4906d1cc4f7d078d43d2f2263beebc95c65969d8
Elmar Hajizade <44324549+Elmar999@users.noreply.github.com>
2019-08-30 11:15:22 -0500

    Update churn_telecom_dataset.py

--- churn_telecom_dataset.py
+++ churn_telecom_dataset.py
@@ -1,4 +1,8 @@
 
+# Customer attrition, also known as customer churn, customer turnover, or customer defection, is the loss of clients or customers.
+#predictive analytics use churn prediction models that predict customer churn by assessing their propensity of risk to churn.
+
+
 # import warnings filter
 from warnings import simplefilter
 # ignore all future warnings
